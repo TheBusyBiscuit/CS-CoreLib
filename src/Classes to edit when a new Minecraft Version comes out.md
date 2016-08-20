f005d378a95596ad507e3ccf6d3c3c3b1b7cb8c9
alekso56 <simple_and_easy@hotmail.com>
2016-08-20 16:14:54 +0200

    Add support for 1.10 - 1.10.2

--- src/Classes to edit when a new Minecraft Version comes out.md
+++ src/Classes to edit when a new Minecraft Version comes out.md
@@ -1,4 +1,5 @@
 * me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.CustomBookOverlay
 * me.mrCookieSlime.CSCoreLibPlugin.general.Chat.TellRawMessage
+* me.mrCookieSlime.CSCoreLibPlugin.general.World.Animals
 
 Add more as you add version-dependent Features
--- src/me/mrCookieSlime/CSCoreLibPlugin/general/World/Animals.java
+++ src/me/mrCookieSlime/CSCoreLibPlugin/general/World/Animals.java
@@ -14,7 +14,7 @@ public class Animals {
 	public static boolean feed(Entity n) throws Exception {
 		if (!isFeedable(n)) return false;
 		Object handle = ReflectionUtils.getHandle(CraftObject.ANIMALS, n);
-		Field f = ReflectionUtils.tryField(ReflectionUtils.getClass(PackageName.NMS, "EntityAnimal"), "bv", "bw");
+		Field f = ReflectionUtils.tryField(ReflectionUtils.getClass(PackageName.NMS, "EntityAnimal"), "bv", "bw","bx");
 		f.setAccessible(true);
 		f.set(handle, 600);
 		return true;
@@ -24,7 +24,7 @@ public class Animals {
 		if (!(n instanceof org.bukkit.entity.Animals && ((Ageable) n).isAdult() && ((Ageable) n).canBreed())) return false;
 		
 		Object handle = ReflectionUtils.getHandle(CraftObject.ANIMALS, n);
-		Field f = ReflectionUtils.tryField(ReflectionUtils.getClass(PackageName.NMS, "EntityAnimal"), "bv", "bw");
+		Field f = ReflectionUtils.tryField(ReflectionUtils.getClass(PackageName.NMS, "EntityAnimal"), "bv", "bw","bx");
 		f.setAccessible(true);
 		return f.getInt(handle) < 1;
 	}
