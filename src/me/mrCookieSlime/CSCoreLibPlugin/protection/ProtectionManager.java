839f38331081bbb0fcf7fa432346b1ba708e924d
BuildTools <aggelikiandgiannis@gmail.com>
2016-09-03 23:08:50 +0200

    Added uSkyblock protection module

--- src/me/mrCookieSlime/CSCoreLibPlugin/protection/ProtectionManager.java
+++ src/me/mrCookieSlime/CSCoreLibPlugin/protection/ProtectionManager.java
@@ -12,12 +12,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import us.talabrek.ultimateskyblock.api.uSkyBlockAPI;
 
 public class ProtectionManager {
-	
+
 	private Config local = new Config("plugins/CS-CoreLib/protection.yml");
 	private Set<ProtectionModule> modules = new HashSet<ProtectionModule>();
-	
+
 	public void registerNewModule(String name, ProtectionModule module) {
 		this.modules.add(module);
 		this.loadModuleMSG(name);
@@ -25,11 +27,11 @@ public class ProtectionManager {
 		local.setDefaultValue("plugins." + name + ".no-block-breaking", "&4&l! &cSorry, but the Plugin &4" + name + " &cdoes not allow you to break this Block");
 		local.save();
 	}
-	
+
 	private void loadModuleMSG(String module) {
 		System.out.println("[CS-CoreLib - Protection] Loaded Protection Module \"" + module + "\"");
 	}
-	
+
 	public ProtectionManager(CSCoreLib cscorelib) {
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("WorldGuard") && cscorelib.getServer().getPluginManager().isPluginEnabled("WorldEdit")) {
 			registerNewModule("WorldGuard", new WorldGuardProtectionModule());
@@ -37,7 +39,7 @@ public class ProtectionManager {
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("Factions")) {
 			registerNewModule("Factions", new FactionsProtectionModule());
 		}
-		if (cscorelib.getServer().getPluginManager().isPluginEnabled("Towny")) {	
+		if (cscorelib.getServer().getPluginManager().isPluginEnabled("Towny")) {
 			registerNewModule("Towny", new TownyProtectionModule());
 		}
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("GriefPrevention")) {
@@ -46,31 +48,34 @@ public class ProtectionManager {
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("ASkyBlock")) {
 			registerNewModule("ASkyBlock", new ASkyBlockProtectionModule());
 		}
-		if(cscorelib.getServer().getPluginManager().isPluginEnabled("LWC")){
+		if (cscorelib.getServer().getPluginManager().isPluginEnabled("LWC")) {
 			registerNewModule("LWC", new LWCProtectionModule());
 		}
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("ProtectionStones")) {
 			this.loadModuleMSG("ProtectionStones");
 		}
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("uSkyblock")) {
-			this.loadModuleMSG("uSkyblock");
+			Plugin plugin = Bukkit.getPluginManager().getPlugin("uSkyBlock");
+            if (plugin instanceof uSkyBlockAPI) {
+                uSkyBlockAPI api = (uSkyBlockAPI) plugin;
+
+            }
 		}
-		
 	}
-	
+
 	public boolean canBuild(UUID uuid, Block b) {
 		return this.canBuild(uuid, b, false);
 	}
-	
+
 	public boolean canAccessChest(UUID uuid, Block b) {
 		return this.canAccessChest(uuid, b, false);
 	}
-	
+
 	public boolean canBuild(UUID uuid, Block b, boolean message) {
 		Player player = Bukkit.getPlayer(uuid);
 		if (player == null) return false;
-		
-		for (ProtectionModule module: modules) {
+
+		for (ProtectionModule module : modules) {
 			if (!module.canBuild(player, b)) {
 				if (message) {
 					player.sendMessage(ChatColor.translateAlternateColorCodes('&', local.getString("plugins." + module.getName() + ".no-block-breaking")));
@@ -78,14 +83,14 @@ public class ProtectionManager {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	public boolean canAccessChest(UUID uuid, Block b, boolean message) {
 		Player player = Bukkit.getPlayer(uuid);
 		if (player == null) return false;
-		
+
 		for (ProtectionModule module: modules) {
 			if (!module.canAccessChest(player, b)) {
 				if (message) {
@@ -94,7 +99,7 @@ public class ProtectionManager {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 
