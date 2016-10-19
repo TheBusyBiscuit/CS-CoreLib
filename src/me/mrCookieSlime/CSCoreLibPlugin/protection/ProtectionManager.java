017df1e83cf468e206da04224455925c0e3683c4
BuildTools <aggelikiandgiannis@gmail.com>
2016-10-19 21:18:19 +0200

    Readded ProtectionStones protection.

--- src/me/mrCookieSlime/CSCoreLibPlugin/protection/ProtectionManager.java
+++ src/me/mrCookieSlime/CSCoreLibPlugin/protection/ProtectionManager.java
@@ -52,6 +52,9 @@ public class ProtectionManager {
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("PreciousStones")) {
 			registerNewModule("PreciousStones", new PreciousStonesProtectionModule());
 		}
+		if(cscorelib.getServer().getPluginManager().isPluginEnabled("ProtectionStones")) {
+            this.loadModuleMSG("ProtectionStones");
+		}
 		if (cscorelib.getServer().getPluginManager().isPluginEnabled("uSkyblock")) {
 			this.loadModuleMSG("uSkyblock");
 		}
--- src/me/mrCookieSlime/CSCoreLibPlugin/protection/modules/PreciousStonesProtectionModule.java
+++ src/me/mrCookieSlime/CSCoreLibPlugin/protection/modules/PreciousStonesProtectionModule.java
@@ -3,7 +3,6 @@ package me.mrCookieSlime.CSCoreLibPlugin.protection.modules;
 import me.mrCookieSlime.CSCoreLibPlugin.protection.ProtectionModule;
 import net.sacredlabyrinth.Phaed.PreciousStones.PreciousStones;
 import net.sacredlabyrinth.Phaed.PreciousStones.field.FieldFlag;
-import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
