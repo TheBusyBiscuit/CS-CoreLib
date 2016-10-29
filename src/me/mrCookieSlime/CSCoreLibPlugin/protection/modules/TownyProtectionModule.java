5a73ab0dce469acb0c31479b0b01195c34ca4706
BuildTools <aggelikiandgiannis@gmail.com>
2016-10-29 20:50:10 +0200

    Made Towny Protection working.

--- src/me/mrCookieSlime/CSCoreLibPlugin/protection/modules/TownyProtectionModule.java
+++ src/me/mrCookieSlime/CSCoreLibPlugin/protection/modules/TownyProtectionModule.java
@@ -1,47 +1,51 @@
 package me.mrCookieSlime.CSCoreLibPlugin.protection.modules;
 
+import com.palmergames.bukkit.towny.object.PlayerCache;
+import com.palmergames.bukkit.towny.object.WorldCoord;
+import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
 import me.mrCookieSlime.CSCoreLibPlugin.protection.ProtectionModule;
-
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.object.Town;
-import com.palmergames.bukkit.towny.object.TownyUniverse;
-import com.palmergames.bukkit.towny.object.WorldCoord;
-
 public class TownyProtectionModule implements ProtectionModule {
 
-	@Override
-	public boolean canBuild(Player player, Block b) {
-		return !isProtected(player, b);
-	}
+    @Override
+    public boolean canBuild(Player player, Block b) {
+        return !isProtected(player, b);
+    }
 
-	@Override
-	public boolean canAccessChest(Player player, Block b) {
-		return !isProtected(player, b);
-	}
+    @Override
+    public boolean canAccessChest(Player player, Block b) {
+        return !isProtected(player, b);
+    }
 
-	private boolean isProtected(Player player, Block b) {
-		try {
-			WorldCoord coord = WorldCoord.parseWorldCoord(b);
-			if (coord.getTownyWorld().hasTownBlock(coord) && coord.getTownBlock().hasTown()) {
-				Town town = coord.getTownBlock().getTown();
-				Resident resident = TownyUniverse.getDataSource().getResident(player.getName());
-				if (resident.hasTown()) {
-					if (town.getUID() != resident.getTown().getUID()) return true;
-				}
-				else return true;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    private boolean isProtected(Player player, Block b) {
+        try {
+            WorldCoord coord = WorldCoord.parseWorldCoord(b);
+            PlayerCache.TownBlockStatus blockStatus = PlayerCacheUtil.getTownBlockStatus(player, coord);
+            switch (blockStatus) {
+                case PLOT_FRIEND:
+                    return false;
+                case PLOT_OWNER:
+                    return false;
+                case UNCLAIMED_ZONE:
+                    return false;
+                case TOWN_RESIDENT:
+                    return false;
+                case NOT_REGISTERED:
+                    return false;
+                default:
+                    return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	@Override
-	public String getName() {
-		return "Towny";
-	}
+    @Override
+    public String getName() {
+        return "Towny";
+    }
 
 }
