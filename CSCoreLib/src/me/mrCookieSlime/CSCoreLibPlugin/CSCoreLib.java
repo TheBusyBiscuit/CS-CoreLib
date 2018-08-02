388b8e73acf49b89e860c4fd187f7b2aac0fdf1c
BuildTools <unconfigured@null.spigotmc.org>
2018-08-02 18:58:33 +0200

    I commented out updater function

--- CSCoreLib/src/me/mrCookieSlime/CSCoreLibPlugin/CSCoreLib.java
+++ CSCoreLib/src/me/mrCookieSlime/CSCoreLibPlugin/CSCoreLib.java
@@ -73,8 +73,8 @@ public class CSCoreLib extends JavaPlugin {
 		cfg.setDefaultValue("skulls.uuid", UUID.randomUUID().toString());
 		cfg.save();
 		
-		utils.setupUpdater(88802, getFile());
-		utils.setupMetrics();
+		//utils.setupUpdater(88802, getFile());
+		//utils.setupMetrics();
 
 		instance = this;
 		
